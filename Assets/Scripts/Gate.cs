f2cc505d075a0f098cb0eb321aad4b2e01eb3d48
Burak Ekemen <bekemen98@gmail.com>
2023-05-07 07:00:50 +0300

    Gate Script

--- Assets/Scripts/Gate.cs
+++ Assets/Scripts/Gate.cs
@@ -2,31 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 
 public class Gate : MonoBehaviour
 {
     public TMP_Text gateText;
+    string[] gateType = { "Sum", "Multiply" };
+    string textOperator;
+    int gateValue;
 
-    private void Awake()
+    void Awake()
     {
-        
-    }
-   
-    void Start()
-    {
-
-    }
-    
-    void Update()
-    {
-
+        gateText.text = $"{SetGateOperator(textOperator)}{SetGateValue(gateValue)}";
     }
 
-    private void OnTriggerEnter(Collider other)
+    private string SetGateOperator(string textOperator)
     {
-        if (other.CompareTag("Gate"))
+        string gateOperator = gateType[Random.Range(0, gateType.Length)];
+
+        if (gateOperator == "Sum")
         {
-
+            return "+";
         }
+        else
+        {
+            return "x";
+        }
+    }
+
+    private int SetGateValue(int gateValue)
+    {
+        gateValue = Random.Range(1, 6);
+        return gateValue;
     }
 }
